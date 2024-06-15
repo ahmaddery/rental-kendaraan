74f63464b8c03085cde6c4a0fb77ca8045b2e769
Dhayyy7 <yogatamadhani93@gmail.com>
2024-06-15 23:35:03 +0700

    halaman reset-pasword

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,24 +4,26 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-
+    {{-- content --}}
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+                <div class="flex flex-wrap">
+                    <div class="w-full lg:w-1/2 p-4">
+                        <div class="max-w-xl">
+                            @include('profile.partials.update-profile-information-form')
+                        </div>
+                    </div>
+                    <div class="w-full lg:w-1/2 p-4">
+                        <div class="max-w-xl">
+                            @include('profile.partials.update-password-form')
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                <div class="p-4 border-t border-gray-200 dark:border-gray-600">
+                    <div class="max-w-xl">
+                        @include('profile.partials.delete-user-form')
+                    </div>
                 </div>
             </div>
         </div>
