03b7afc5b7a2fef1f92942ddefa544164c2bcdfd
Nityasa Tustika <tustika.nityasa@gmail.com>
2024-06-17 02:45:40 +0700

    mengganti tampilan sewa produk

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,10 +1,7 @@
 @include('layouts.navbar')
 
-<div class="section">
+<div class="section kendaraan" data-aos="fade-up" data-aos-duration="1000">
     <div class="container">
-        <div class="row align-items-center mt-5">
-            <h2 class="pt-5"> <b>Sewa Kendaraan</b></h2>
-        </div>
 
         <!-- Alert Messages -->
         <div class="row mt-4">
@@ -43,48 +40,98 @@
             </div>
         </div>
 
-        <!-- Product Cards -->
-        <div class="row g-3">
-            <!-- Start Column 1 -->
-            @if ($kendaraans->isEmpty())
-            <div class="col-md-12">
-                <p>Belum ada kendaraan.</p>
-            </div>
-            @else
-            @foreach($kendaraans as $kendaraan)
-            <div class="col-12 col-md-6 col-lg-4 mb-4">
-                <div class="card card-product h-100 border-0 shadow">
-                    <div class="card-header">
-                        <img src="{{ $kendaraan->image }}" class="card-img-top rounded-3" alt="{{ $kendaraan->nama }}" loading="lazy">
-                    </div>
-                    <div class="card-body">
-                        <div class="head-container">
-                            <h2 class="card-title">{{ $kendaraan->nama }}</h2>
-                            <h2 class="text-secondary">{{ $kendaraan->tahun }}</h2>
-                        </div>
-                        <p class="card-type">{{ $kendaraan->brand->kendaraan }}</p>
-                        <ul class="list-inline mb-4 mt-3">
-                            <li>{{ $kendaraan->plat_nomor }}</li>
-                            <li>{{ $kendaraan->category->kendaraan}}</li>
-                            <li>{{ $kendaraan->type->typekendaraan }}</li>
-                            <li>{{ $kendaraan->warna }}</li>
-                        </ul>
-                        <hr style="border-top:3px solid black">
-                        <div class="price-container mt-2">
-                            <h1>IDR</h1>
-                            <h1 class="ml-auto">{{ number_format($kendaraan->harga, 0, ',', '.') }}</h1>
-                        </div>
-                        <div class="button-group mt-5">
-                            <a href="{{ route('kendaraan.detail', $kendaraan->id) }}" class="btn btn-primary btn-detail">Detail</a>
-                            <a href="{{ route('tambah.keranjang', $kendaraan->id) }}" class="btn btn-primary btn-cart"><i class="bi bi-cart"></i></a>
+        <!-- Start Sewa Kendaraan -->
+        <!-- Start Title Section -->
+        <div class="mb-4">
+            <div class="sub-title" id="sewa">Sewa Kendaraan</div>
+                <div class="row">
+                    <div class="col-xl-7 col-lg-9">
+                        <div class="title-desc">
+                            <p>Sewa kendaraan berharga dengan harga bersahabat</p>
                         </div>
                     </div>
                 </div>
             </div>
-            @endforeach
-            @endif
-            <!-- End Column 1 -->
+            @if (session('success'))
+            <script>
+            Swal.fire({
+                toast: true
+                , position: 'top-end'
+                , icon: 'success'
+                , title: '{{ session('
+                success ') }}'
+                , showConfirmButton: false
+                , timer: 6000
+                , timerProgressBar: true
+            , });
+            </script>
+        @endif
+        @if (session('error'))
+        <script>
+            Swal.fire({
+                toast: true
+                , position: 'top-end'
+                , icon: 'error'
+                , title: '{{ session('
+                error ') }}'
+                , showConfirmButton: false
+                , timer: 6000
+                , timerProgressBar: true
+            , });
+
+        </script>
+        @endif
+        <!-- End Title Section -->
+
+        @if ($kendaraans->isEmpty())
+        <div class="col-md-12">
+            <p>Belum ada kendaraan.</p>
         </div>
+        @else
+        <div class="d-flex flex-wrap">
+            @foreach($kendaraans as $kendaraan)
+
+            <!-- Start Product Column -->
+            <div class="col-12 col-md-6 col-lg-4 mb-4">
+                <div class="card card-vehicle">
+                    <img src="{{ $kendaraan->image }}" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <div class="card-title-year">
+                            <ul>
+                                <li class="card-title">{{ $kendaraan->nama }}</li>
+                                <li class="card-year">{{ $kendaraan->tahun }}</li>
+                            </ul>
+                        </div>
+                        <p class="card-type">{{ $kendaraan->brand->kendaraan }}</p>
+                        <div class="card-specs mt-3">
+                            <ul>
+                                <li><i class="bi bi-people"></i> {{ $kendaraan->plat_nomor }}</li>
+                                <li><i class="bi bi-shield-check"></i> {{ $kendaraan->category->kendaraan}}</li>
+                                <li><i class="bi bi-car-front"></i> {{ $kendaraan->type->typekendaraan }}</li>
+                                <li><i class="bi bi-suitcase-lg"></i> {{ $kendaraan->warna }}</li>
+                            </ul>
+                        </div>
+                        <hr class="mt-4">
+                        <div class="card-price mb-2">
+                            <ul>
+                                <li class="idr">Rp</li>
+                                <li class="idrnom">{{ number_format($kendaraan->harga, 2, ',', '.') }}</li>
+                            </ul>
+                        </div>
+                        <div class="btn-card mt-3">
+                            <a href="{{ route('kendaraan.detail', $kendaraan->id) }}" class="btn me-2">Detail</a>
+                            <a href="{{ route('tambah.keranjang', $kendaraan->id) }}" class="btn"><i class="bi bi-cart2"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Product Column -->
+            @endforeach
+        </div>
+        @endif
+        </div>
+        </div>
+        <!-- End Sewa Kendaraan -->
     </div>
 </div>
 
