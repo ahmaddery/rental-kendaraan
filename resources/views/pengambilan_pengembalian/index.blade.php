4ceea5141ff3613b150dbcc29587a38cf5493541
Dery <211110020@student.mercubuana-yogya.ac.id>
2024-06-18 10:54:15 +0700

    Added Bootstrap Bundle script and removed newline at end of file in index.blade.php

--- resources/views/pengambilan_pengembalian/index.blade.php
+++ resources/views/pengambilan_pengembalian/index.blade.php
@@ -146,3 +146,5 @@
     </div>
 </div>
 @include('layouts.modal')
+<!-- Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
